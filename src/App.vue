1c6efb38b00d08e02254a6a473e37c4035319393
arrahmna-dev <dra390@gmail.com>
2023-08-20 22:36:09 +0600

    from m8 projects

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+	import ParentComponent from "./components/ParentComponent.vue"
+</script>
+
+<template>
+	<div>
+		<ParentComponent />
+	</div>
+</template>
+
+<style scoped></style>
+
